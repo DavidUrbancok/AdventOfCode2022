e7362653f523ec11df7f745d836367cdbec9d767
Dávid Urbančok <urbancok.david@outlook.com>
2022-12-01 18:35:21 +0100

    Added Day 1 solutions

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,55 @@
+﻿var input = File.ReadAllLines("input.txt");
+var currentCalories = 0;
+
+// ============================
+// ========== PART 1 ==========
+// ============================
+
+var maxCalories = 0;
+
+foreach (var line in input)
+{
+    if (line.Equals(string.Empty))
+    {
+        if (currentCalories > maxCalories)
+        {
+            maxCalories = currentCalories;
+        }
+        
+        currentCalories = 0;
+
+        continue;
+    }
+
+    currentCalories += int.Parse(line);
+}
+
+Console.WriteLine(maxCalories);
+
+// ============================
+// ========== PART 2 ==========
+// ============================
+
+currentCalories = 0;
+IList<int> calories = new List<int>();
+
+foreach (var line in input)
+{
+    if (line.Equals(string.Empty))
+    {
+        calories.Add(currentCalories);
+
+        currentCalories = 0;
+
+        continue;
+    }
+
+    currentCalories += int.Parse(line);
+}
+
+var result = calories
+    .OrderByDescending(calorie => calorie)
+    .Take(Range.EndAt(3))
+    .Sum();
+
+Console.WriteLine(result);
