c614ff26c56d44f13ed3c4f70bf57d888980df87
Dávid Urbančok <6781066+DavidUrbancok@users.noreply.github.com>
2022-12-05 23:11:34 +0100

    Added day 3 Part 2 and Day 4 solutions

--- src/Day3/Program.cs
+++ src/Day3/Program.cs
@@ -15,3 +15,27 @@ var totalPriority = (from line in input
     select aToZ.IndexOf(commonLetter) + 1).Sum();
 
 Console.WriteLine(totalPriority);
+
+// ============================
+// ========== PART 2 ==========
+// ============================
+
+totalPriority = 0;
+
+for (var i = 0; i < input.Length / 3; i++)
+{
+    var lines = new string[3];
+
+    for (var j = 0; j < 3; j++)
+    {
+        lines[j] = input[i * 3 + j];
+    }
+
+    var commonLetter = lines[0].
+        First(character => 
+            lines[1].Contains(character) && lines[2].Contains(character));
+
+    totalPriority += aToZ.IndexOf(commonLetter) + 1; 
+}
+
+Console.WriteLine(totalPriority);
---
+++ src/Day4/Program.cs
@@ -0,0 +1,46 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+// ============================
+// ========== PART 1 ==========
+// ============================
+
+var overlappingRanges = (from line in input
+    select line.Split(',')
+    into pairs
+    let firstElf = pairs[0].Split('-')
+    let secondElf = pairs[1].Split('-')
+    let firstRange = new Range(int.Parse(firstElf[0]), int.Parse(firstElf[1]))
+    let secondRange = new Range(int.Parse(secondElf[0]), int.Parse(secondElf[1]))
+    where firstRange.Start.Value <= secondRange.Start.Value && firstRange.End.Value >= secondRange.End.Value ||
+          secondRange.Start.Value <= firstRange.Start.Value && secondRange.End.Value >= firstRange.End.Value
+    select firstRange).Count();
+
+Console.WriteLine(overlappingRanges);
+
+// ============================
+// ========== PART 2 ==========
+// ============================
+
+overlappingRanges = default;
+
+foreach (var line in input)
+{
+    var pairs = line.Split(',');
+    var firstElf = pairs[0].Split('-');
+    var secondElf = pairs[1].Split('-');
+
+    var firstRange = new Range(int.Parse(firstElf[0]), int.Parse(firstElf[1]));
+    var secondRange = new Range(int.Parse(secondElf[0]), int.Parse(secondElf[1]));
+
+    if (firstRange.Start.Value == secondRange.Start.Value || firstRange.End.Value == secondRange.End.Value)
+    {
+        ++overlappingRanges;
+    }
+    else if (firstRange.Start.Value < secondRange.Start.Value && firstRange.End.Value >= secondRange.Start.Value
+          || firstRange.Start.Value > secondRange.Start.Value && secondRange.End.Value >= firstRange.Start.Value)
+    {
+        ++overlappingRanges;
+    }
+}
+
+Console.WriteLine(overlappingRanges);
