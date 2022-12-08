5e4161f27225607c69886e08d174cdbc7572ea44
Dávid Urbančok <6781066+DavidUrbancok@users.noreply.github.com>
2022-12-08 22:22:39 +0100

    Added Day 7 solutions

---
+++ src/Day7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+// ============================
+// ========== PART 1 ==========
+// ============================
+
+Stack<string> path = new();
+Dictionary<string, int> sizes = new();
+
+foreach (var line in input)
+{
+    if (line == "$ cd ..")
+    {
+        path.Pop();
+        continue;
+    }
+    
+    if (line.StartsWith("$ cd"))
+    {
+        path.Push(string.Join(string.Empty, path) + line.Split(" ")[2]);
+        continue;
+    }
+    
+    if (Regex.Match(line, "\\d+").Success)
+    {
+        var fileSize = int.Parse(line.Split(" ")[0]);
+
+        foreach (var directory in path)
+        {
+            sizes[directory] = sizes.GetValueOrDefault(directory) + fileSize;
+        }
+    }
+}
+
+Console.WriteLine(sizes.Values.Where(value => value <= 100_000).Sum());
+
+// ============================
+// ========== PART 2 ==========
+// ============================
+
+var sum = sizes.Values.Max();
+var freeSpace = 70_000_000 - sum;
+
+Console.WriteLine(sizes.Values.Where(size => size + freeSpace >= 30_000_000).Min());
