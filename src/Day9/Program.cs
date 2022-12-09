dd757e23eac2599b287715c10addff88f734e7da
Dávid Urbančok <6781066+DavidUrbancok@users.noreply.github.com>
2022-12-10 00:12:59 +0100

    Refactored Day 9 solution

--- src/Day9/Program.cs
+++ src/Day9/Program.cs
@@ -4,12 +4,10 @@
 // ========== PART 1 ==========
 // ============================
 
-var visited = new HashSet<StackTuple>();
+var visited = new HashSet<Tuple>();
 
-var hX = 0;
-var hY = 0;
-var tX = 0;
-var tY = 0;
+var snakeHead = new Point { X = 0, Y = 0 };
+var snakeTail = new Point { X = 0, Y = 0 };
 
 
 foreach (var line in input)
@@ -18,67 +16,14 @@ foreach (var line in input)
     var direction = strings[0];
     var amount = int.Parse(strings[1]);
 
-    int horizontalDiff;
-    int verticalDiff;
-
     switch (direction)
     {
         case "U":
         {
             for (var i = 0; i < amount; i++)
             {
-                hY++;
-
-                horizontalDiff = hX - tX;
-                verticalDiff = hY - tY;
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 1
-                    || Math.Abs(horizontalDiff) == 1 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 0)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 0 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                visited.Add(new StackTuple(tX, tY));
+                snakeHead.Y++;
+                ProcessMovement(snakeHead, snakeTail);
             }
 
             break;
@@ -87,58 +32,8 @@ foreach (var line in input)
         {
             for (var i = 0; i < amount; i++)
             {
-                hY--;
-
-                horizontalDiff = hX - tX;
-                verticalDiff = hY - tY;
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 1
-                    || Math.Abs(horizontalDiff) == 1 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 0)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 0 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                visited.Add(new StackTuple(tX, tY));
+                snakeHead.Y--;
+                ProcessMovement(snakeHead, snakeTail);
             }
 
             break;
@@ -147,59 +42,8 @@ foreach (var line in input)
         {
             for (var i = 0; i < amount; i++)
             {
-                hX--;
-
-                horizontalDiff = hX - tX;
-                verticalDiff = hY - tY;
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 1
-                    || Math.Abs(horizontalDiff) == 1 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 0)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 0 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                visited.Add(new StackTuple(tX, tY));
-
+                snakeHead.X--;
+                ProcessMovement(snakeHead, snakeTail);
             }
 
             break;
@@ -208,58 +52,8 @@ foreach (var line in input)
         {
             for (var i = 0; i < amount; i++)
             {
-                hX++;
-
-                horizontalDiff = hX - tX;
-                verticalDiff = hY - tY;
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 1
-                    || Math.Abs(horizontalDiff) == 1 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 0)
-                {
-                    if (horizontalDiff < 0)
-                    {
-                        tX--;
-                    }
-                    else
-                    {
-                        tX++;
-                    }
-                }
-
-                if (Math.Abs(horizontalDiff) == 0 && Math.Abs(verticalDiff) == 2)
-                {
-                    if (verticalDiff < 0)
-                    {
-                        tY--;
-                    }
-                    else
-                    {
-                        tY++;
-                    }
-                }
-
-                visited.Add(new StackTuple(tX, tY));
+                snakeHead.X++;
+                ProcessMovement(snakeHead, snakeTail);
             }
 
             break;
@@ -269,4 +63,48 @@ foreach (var line in input)
 
 Console.WriteLine(visited.Count);
 
-internal record StackTuple(int X, int Y);
+static void HandleMovement(ref int point, int diff)
+{
+    if (diff < 0)
+    {
+        point--;
+    }
+    else
+    {
+        point++;
+    }
+}
+
+void ProcessMovement(Point head, Point tail)
+{
+    var horizontalDiff = head.X - tail.X;
+    var verticalDiff = head.Y - tail.Y;
+
+    if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 1
+        || Math.Abs(horizontalDiff) == 1 && Math.Abs(verticalDiff) == 2)
+    {
+        HandleMovement(ref tail.X, horizontalDiff);
+        HandleMovement(ref tail.Y, verticalDiff);
+    }
+
+    if (Math.Abs(horizontalDiff) == 2 && Math.Abs(verticalDiff) == 0)
+    {
+        HandleMovement(ref tail.X, horizontalDiff);
+    }
+
+    if (Math.Abs(horizontalDiff) == 0 && Math.Abs(verticalDiff) == 2)
+    {
+        HandleMovement(ref tail.Y, verticalDiff);
+    }
+
+    visited.Add(new Tuple(tail.X, tail.Y));
+}
+
+internal record Tuple(int X, int Y);
+
+internal class Point
+{
+    internal int X;
+
+    internal int Y;
+}
